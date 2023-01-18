2aea55d10e8fca0a4cddc567473cc29dcc75279b
Kati89 <kovacs.katalin@sapientia.ms.ro>
2023-01-18 10:39:58 -1000

    repo

--- src/main/java/ro/mycode/televizorapi/controller/TelevizorController.java
+++ src/main/java/ro/mycode/televizorapi/controller/TelevizorController.java
@@ -3,6 +3,7 @@ package ro.mycode.televizorapi.controller;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ro.mycode.televizorapi.model.Televizor;
 import ro.mycode.televizorapi.repository.TelevizorRepo;
@@ -25,9 +26,17 @@ public class TelevizorController {
 
     @GetMapping("api/v1/televizoare/marci")
     public List<String>getAllMarci(){
+
         return  televizorRepo.getAllMarci();
+    }
+    @GetMapping("api/v1/televizoare/{marca}")
+    public  List<Televizor> getAllTelevizoareByMarca(@PathVariable String marca){
+        return  televizorRepo.getAllTelevizorById(marca);
+
     }
 
 }
+
+
 
 
